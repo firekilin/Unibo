b15f81e8fb9b36d25c537e85ccc513b105ff44ae
firekilin <wl00161839@gmail.com>
2020-12-18 02:08:15 +0800

    no message

--- public/js/product.js
+++ public/js/product.js
@@ -7,13 +7,15 @@ $(() => {
       $.post("/api/getmessage", { 
         mess:$("#message").val(),
       }, (data, status) => {
-        $("#uniboface")[0].src=data.uniboFace;
+        
         for(let i=0;i<data.anser.length;i++){
           if(data.anser[i]=='null'){
             let anns="https://www.dcard.tw/f/cycu<br>https://www.facebook.com/CYCU.PR/"
             $("#textBox").append("<div class='line'><img src='./public/img/unibo/cry_motion2.gif' class='round_icon cont2' ><div class='other'>查無資料<br>可以到相關網站進行詢問喔。<br>"+anns+"<br>也可以幫我們填寫回饋單使我們增加題庫</div></div>");
+            $("#uniboface")[0].src="./public/img/unibo/cry.gif";
           }else{
             $("#textBox").append("<div class='line'><img src='./public/img/unibo/standard2.gif' class='round_icon cont2' ><div class='other'>"+data.anser[i].qs+"<br>"+data.anser[i].ans+"</div></div>");
+            $("#uniboface")[0].src=data.uniboFace;
           }
          
         }
