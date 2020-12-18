0821c8755ad56ddfb6780573717060910679344c
firekilin <wl00161839@gmail.com>
2020-12-18 18:34:00 +0800

    no message

--- public/js/product.js
+++ public/js/product.js
@@ -3,7 +3,11 @@
 var unibo=this.unibo?(()=>{throw new Error()})():{};
 $(() => {
     $("#gogo").click(() =>{
+
       $("#textBox").append("<div class='line'><img src='./public/img/users.png' class='round_icon cont' ><div class='us'>"+$("#message").val()+"</div></div>");
+     
+      $("#textBox").append("<div class='line' id='loading'><img src='./public/img/unibo/standard2.gif' class='round_icon cont2' ><div class='other dotloading'><span class='dot'></span><span class='dot'></span><span class='dot'></span></div></div>");
+      
       $.post("/api/getmessage", { 
         mess:$("#message").val(),
       }, (data, status) => {
@@ -11,8 +15,10 @@ $(() => {
         for(let i=0;i<data.anser.length;i++){
           if(data.anser[i]=='null'){
             let anns="https://www.dcard.tw/f/cycu<br>https://www.facebook.com/CYCU.PR/"
+            
             $("#textBox").append("<div class='line'><img src='./public/img/unibo/cry_motion2.gif' class='round_icon cont2' ><div class='other'>查無資料<br>可以到相關網站進行詢問喔。<br>"+anns+"<br>也可以幫我們填寫回饋單使我們增加題庫</div></div>");
             $("#uniboface")[0].src="./public/img/unibo/cry.gif";
+            
           }else{
             let ans=data.anser[i].ans;
             let htmfirst=ans.indexOf("http");
@@ -34,7 +40,7 @@ $(() => {
               
             }
        
-
+            
             $("#textBox").append("<div class='line'><img src='./public/img/unibo/standard2.gif' class='round_icon cont2' ><div class='other'>"+data.anser[i].qs+"<br>"+showans+"</div></div>");
             $("#uniboface")[0].src=data.uniboFace;
           }
@@ -46,7 +52,7 @@ $(() => {
             $("#getquestion").append(`<div class='questionitem' onclick="unibo.sendmsg('`+data.qustion[i]+`')">`+data.qustion[i]+`</div>`)
           }
         }
-        
+        $("#loading").remove();
         $('#textBox').animate({ scrollTop:$('#textBox')[0].scrollHeight }, 800);
         });
     });
